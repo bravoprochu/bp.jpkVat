1aaf63a381727485e99e4b5df1a02352f5aca44a
Bartosz Prochowski <bravoprochu@gmail.com>
2018-02-26 16:24:23 +0100

    fix nip validation ("brak" if not last 4 numbers)

--- jpkVat/JpkPodmiot.cs
+++ jpkVat/JpkPodmiot.cs
@@ -8,14 +8,20 @@ namespace bp.jpkVat
 {
     public class JpkPodmiot
     {
+        [MinLength(4)]
+        private string _nip {get;set;}
         [MaxLength(10)]
-        public string NIP { get; set; }
+        public string NIP { get {
+                string res = "";
+                int v = -1;
+                bool canParse=Int32.TryParse(_nip.TakeLast(4).ToString(), out v);
+                return canParse ? _nip : "brak";
+            } set { this._nip = value; } }
         private string pelnaNazwa;
         public string PelnaNazwa
         {
             get
             {
-
                 return pelnaNazwa.Replace((char)34, (char)36);
             }
             set { pelnaNazwa = value; }

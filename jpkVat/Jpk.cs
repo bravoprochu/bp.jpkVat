da743b01a94e24db07b2941ed788fe3c0821ffde
Bartosz Prochowski <bravoprochu@gmail.com>
2018-02-27 13:26:44 +0100

    fix czwarty rzadz pusty

--- jpkVat/Jpk.cs
+++ jpkVat/Jpk.cs
@@ -28,10 +28,10 @@ namespace bp.jpkVat
             var z = this.Zakup;
             sb.AppendLine(@"KodFormularza;kodSystemowy;wersjaSchemy;WariantFormularza;CelZlozenia;DataWytworzeniaJPK;DataOd;DataDo;NazwaSystemu;NIP;PelnaNazwa;Email;LpSprzedazy;NrKontrahenta;NazwaKontrahenta;AdresKontrahenta;DowodSprzedazy;DataWystawienia;DataSprzedazy;K_10;K_11;K_12;K_13;K_14;K_15;K_16;K_17;K_18;K_19;K_20;K_21;K_22;K_23;K_24;K_25;K_26;K_27;K_28;K_29;K_30;K_31;K_32;K_33;K_34;K_35;K_36;K_37;K_38;K_39;LiczbaWierszySprzedazy;PodatekNalezny;LpZakupu;NrDostawcy;NazwaDostawcy;AdresDostawcy;DowodZakupu;DataZakupu;DataWplywu;K_43;K_44;K_45;K_46;K_47;K_48;K_49;K_50;LiczbaWierszyZakupow;PodatekNaliczony;");
             sb.AppendLine($"JPK_VAT;{n.KodFormularza.KodSystemowy};{n.KodFormularza.WersjaSchemy};{n.WariantFormularza};{n.CelZlozenia};{n.DataWytworzeniaJPK.ToString(dateTimeClassicFormat)};{n.DataOd.ToString(dateClassicFormat)};{n.DataDo.ToString(dateClassicFormat)};{n.NazwaSystemu}");
-            //
             var pominieteKolumny = new string(';', 9); //9 kolumn
             sb.AppendLine($"{pominieteKolumny}{StringHelpful.Nip10LastChars(p.NIP)};{p.PelnaNazwa};{p.Email}");
-
+            //pusty czwarty rząd
+            sb.AppendLine();
             pominieteKolumny = new string(';', 12); //12 kolumny
             foreach (var w in s.SprzedazWiersz)
             {
